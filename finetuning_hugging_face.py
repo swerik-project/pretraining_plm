ad32cd79c70437cf7411a6707acdbb30422e603b
LaurineMir <laurinemeier@gmail.com>
2024-04-16 13:37:47 +0200

    restarting the finetuning on the model with whitespaces

--- finetuning_hugging_face.py
+++ finetuning_hugging_face.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pickle
 import preprocessing
 import argparse
-
+from datasets import load_dataset
 
 
 
@@ -21,7 +21,7 @@ def main(args):
     model = preprocessing.create_model_MLM(args.model_checkpoint)
     tokenizer =preprocessing.create_tokenizer(model_checkpoint)
 
-    #data_files = {"train": "swerick_data_long_train.pkl", "test": "swerick_data_long_test.pkl"}
+    #data_files = {"train": "swerick_data_random_train.pkl", "test": "swerick_data_random_test.pkl"}
     #swerick_dataset = load_dataset("pandas",data_files=data_files)
     #tokenized_datasets =preprocessing.tokenize_dataset(swerick_dataset,tokenizer)
     #lm_datasets = preprocessing.grouping_dataset(tokenized_datasets,chunk_size)
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_filename", type=str, default="KBLab/bert-base-swedish-cased", help="Save location for the model")
     parser.add_argument("--model_checkpoint", type=str, default="KBLab/bert-base-swedish-cased", help="Save location for checkpoint of the trainer")
     parser.add_argument("--checkpoint_trainer", type=str, default=None, help="Save location for checkpoint of the trainer")
-    parser.add_argument("--name", type=str, default="finetuning_hugging_python", help="repository name")
+    parser.add_argument("--name", type=str, default="finetuning_hugging_whitespace", help="repository name")
     parser.add_argument("--chunk_size", type=int, default=128)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--epochs", type=int, default=100)
